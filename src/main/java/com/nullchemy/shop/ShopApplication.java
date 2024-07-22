54fbf4c0efe9017675d87e8e31c78261effe4428
DennisRono <dennisrkibet@gmail.com>
2024-07-22 14:25:48 +0300

    starter files for my first spring boot server

---
+++ src/main/java/com/nullchemy/shop/ShopApplication.java
@@ -0,0 +1,25 @@
+package com.nullchemy.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ShopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Homepage %s!", name);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello again %s!", name);
+	}
+}
